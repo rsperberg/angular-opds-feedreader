bedf6660e860c2241ca700307c65446bef04512a
rsperberg <rsperberg@gmail.com>
2015-01-15 22:24:16 -0500

    add code advised at enable-cors

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+// from http://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
